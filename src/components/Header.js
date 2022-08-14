bc7befd443fa40cb7af6e5623faeb0b207e5d284
dragi-ns <54716240+dragi-ns@users.noreply.github.com>
2022-08-14 13:00:45 +0200

    Initialize project

---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+function Header() {
+  return <header className="app-header">Header</header>;
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,5 @@
+function Main() {
+  return <main className="app-main">Main</main>;
+}
+
+export default Main;
